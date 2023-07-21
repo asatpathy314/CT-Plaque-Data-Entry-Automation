1f9afc3aa2fc4d3757c7a5a922318aeae3893b2a
asatpathy314 <137309541+asatpathy314@users.noreply.github.com>
2023-07-21 11:18:06 -0400

    Commented the code, deleted redundant bug-testing functions.

--- app/data.py
+++ app/data.py
@@ -6,24 +6,25 @@ import re
 import shutil
 import os
 
-
+#Convert text to REDCap data value
 def translateTextToIdentifier(inputVessel, lookupTable):
     outputString = "12"
-
     for row in lookupTable:
         if inputVessel.lower() == row[1].lower():
             outputString = row[0]
             return int(outputString)
     return int(outputString)
 
+#Return column of a 2D array as a 1D array
 def returnColAsArray(TwoDArray, n):
     return [i[n] for i in TwoDArray]
 
+#Remove Non-Numeric characters from a string (periods are kept as decimals)
 def removeNonNumeric(inputString):
     pattern = r"[^\d.]"
     return re.sub(pattern, "", inputString)
 
-
+#Convert a .csv file to a 2D Array (yes I know I could've used csv.reader() I'm just an idiot)
 def convertTo2DArray(csvFilePath):
     csvArray = [i.split(",") for i in open(csvFilePath, "r").readlines()]
     def addBlank(arr1D, targetLen):
@@ -35,41 +36,48 @@ def convertTo2DArray(csvFilePath):
     csvArray = [list(map(strip, addBlank(i, len(csvArray[0])))) for i in csvArray]
     return csvArray
 
-
+#Get rid of the elements in .txt input that don't need to be in array.
 def stripArrayElements(arr):
     strippedArray = [element.strip() for element in arr if element != "\n" and ":" not in element]   
     return strippedArray
 
-
+#Populate the .csv database import format with the input from the .txt file
 def update_csv_file(input_file_path, format_file_path, target_record_id):
+    
     lookupTable = [["1", "D1"], ["2", "D2"], ["3", "D3"], ["4", "OM1"], ["5", "OM2"], ["6", "OM3"], ["7", "Ramus"],
                    ["8", "L-PLB"], ["9", "L-PDA"], ["10", "R-PLB"], ["11", "R-PDA"], ["13", "LM"], ["14", "LAD"],
                    ["15", "LCX"], ["16", "RCA"]]
+    
     txtArrayList = [i for i in open(input_file_path, 'r').readlines()]
     txtArrayList = stripArrayElements(txtArrayList)
     csv2DArray = convertTo2DArray(format_file_path)
-    
+
+    #Find target column
     try:
         targetColumn = csv2DArray[1].index(target_record_id)
     except ValueError:
         print("Invalid record ID, check that the record ID is present and spelled correctly in the format .csv file")
-    print(txtArrayList)
+    
+    #Initialize tracker variables
     indexOfLoggedData = []
     otherCounter = 0
     csvFirstCol = [i[0] for i in csv2DArray]
-    
     i=0
+
+    #Begin while loop
     while i < len(txtArrayList):
         tempIndex = translateTextToIdentifier(txtArrayList[i], lookupTable)
+        
+        #For any of the four major vessels. Feel free to remove this and edit the lookup table accordingly if you would rather treat all the vessels the same.
         if tempIndex in range(13, 17):
             for j in range(csvFirstCol.index("ncpvolume_"+str(txtArrayList[i]).lower()), csvFirstCol.index("ncpvolume_"+str(txtArrayList[i]).lower())+26):
                 i+=1
-                print(txtArrayList[i])
                 if csv2DArray[j][targetColumn] != "":
                     csv2DArray[j][targetColumn] = str(float(csv2DArray[j][targetColumn]) + float(removeNonNumeric(txtArrayList[i])))
                 else:
                     csv2DArray[j][targetColumn] = removeNonNumeric(txtArrayList[i])
-                
+
+        #Any other less commonly contoured vessels
         else:
            otherIndex = csvFirstCol.index("ncpvolume_" + str(otherCounter+1))
            csv2DArray[otherIndex-2][targetColumn] = str(tempIndex)
@@ -81,11 +89,9 @@ def update_csv_file(input_file_path, format_file_path, target_record_id):
                 otherIndex = csvFirstCol.index("ncpvolume_" + str(indexOfLoggedData.index(txtArrayList[i])+1))
                 for j in range(otherIndex, otherIndex+26):
                     i += 1
-                    print(txtArrayList[i])
                     csv2DArray[j][targetColumn] = str(float(csv2DArray[j][targetColumn])+float(removeNonNumeric(txtArrayList[i])))   
            else:
                 for j in range(otherIndex, otherIndex+26):
-                    print(txtArrayList[i])
                     i+=1
                     csv2DArray[j][targetColumn] = removeNonNumeric(txtArrayList[i])
            otherCounter += 1
@@ -100,6 +106,11 @@ def update_csv_file(input_file_path, format_file_path, target_record_id):
     
 
 
+#GUI Maker
+def start_program():
+    input_file_path = input_file_entry.get()
+    format_file_path = format_file_entry.get()
+    target_record_id = record_id_entry.get()
 
 def browse_input_file():
     file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
@@ -112,9 +123,6 @@ def browse_format_file():
     format_file_entry.delete(0, tk.END)
     format_file_entry.insert(0, file_path)
 
-
-
-# Rest of the code...
 def download_file():
     input_file_path = input_file_entry.get()
     format_file_path = format_file_entry.get()
@@ -135,6 +143,7 @@ def download_file():
     # Remove the temporary file
     os.remove(temp_file_path)
 
+#Initialization
 root = tk.Tk()
 root.title("CT Data Automaton")
 root.resizable(False,False)
